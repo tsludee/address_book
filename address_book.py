ea9ff97c30c2444e489c5c8ca152f61a728ffc98
Taylor Sluder <tsludee@gmail.com>
2023-07-09 17:25:21 -0400

    Update address_book.py
    added a view button to see a record without editing it. still need to work on database creation. is there a way to check if the database itself has items and then it won't create the database again? i also want to add a notes section

--- address_book.py
+++ address_book.py
@@ -13,8 +13,12 @@ conn = sqlite3.connect('address_book.db')
 #create cursor
 c = conn.cursor()
 
-# create table
-c.execute("""CREATE TABLE addresses (
+# create database/table
+address_book = 'address_book.db'
+check_file = os.path.isfile(address_book)
+# print(check_file)
+if check_file != True:
+    c.execute("""CREATE TABLE addresses (
           first_name text,
           last_name text,
           address text,
@@ -22,6 +26,7 @@ c.execute("""CREATE TABLE addresses (
           state text,
           zipcode integer
           )""")
+
 
 # create submit function for database
 def submit():
@@ -72,6 +77,77 @@ def query():
     #close connection
     conn.close()
 
+# create view function
+def view():
+    global viewer
+    viewer = Tk()
+    viewer.title('Record Viewer') #maybe i can find a way to have the name of the record appear as the title
+    viewer.geometry('400x400')
+     # connect to database from inside your function!
+    conn = sqlite3.connect('address_book.db')
+    # create cursor inside function, too!
+    c = conn.cursor()
+
+    record_id = delete_box.get()
+    # query the database
+    c.execute("SELECT * FROM addresses WHERE oid= " + record_id)
+    records = c.fetchall()
+
+    first_name = ''
+    last_name = ''
+    street_address = ''
+    city_name = ''
+    state_name = ''
+    zipcode_number = ''
+    # loop through results
+    for record in records:
+        first_name = record[0]
+        last_name = record[1]
+        street_address = record[2]
+        city_name = record[3]
+        state_name = record[4]
+        zipcode_number = str(record[5])
+
+    # create text boxes
+    f_name_viewer = Label(viewer, text=first_name)
+    f_name_viewer.grid(row=0, column=1, padx=20, pady=(10, 0))
+    l_name_viewer = Label(viewer, text=last_name)
+    l_name_viewer.grid(row=1, column=1)
+    address_viewer = Label(viewer, text=street_address)
+    address_viewer.grid(row=2, column=1)
+    city_viewer = Label(viewer, text=city_name)
+    city_viewer.grid(row=3, column=1)
+    state_viewer = Label(viewer, text=state_name)
+    state_viewer.grid(row=4, column=1)
+    zipcode_viewer = Label(viewer, text=zipcode_number)
+    zipcode_viewer.grid(row=5, column=1)
+    
+    # create text box labels
+    f_name_label_viewer = Label(viewer, text="First Name: ")
+    f_name_label_viewer.grid(row=0, column=0, pady=(10, 0))
+    l_name_label_viewer = Label(viewer, text="Last Name: ")
+    l_name_label_viewer.grid(row=1, column=0)
+    address_label_viewer = Label(viewer, text="Street Address: ")
+    address_label_viewer.grid(row=2, column=0)
+    city_label_viewer = Label(viewer, text="City: ")
+    city_label_viewer.grid(row=3, column=0)
+    state_label_viewer = Label(viewer, text="State: ")
+    state_label_viewer.grid(row=4, column=0)
+    zipcode_label_viewer = Label(viewer, text="Zipcode: ")
+    zipcode_label_viewer.grid(row=5, column=0)
+
+    close_btn = Button(viewer, text="Close Record", command=close)
+    close_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=145)
+
+    #commit changes
+    conn.commit()
+    #close connection
+    conn.close()
+
+# create close function
+def close():
+    viewer.destroy()
+
 # create edit function
 def update():
     # connect to database from inside your function!
@@ -105,7 +181,6 @@ def update():
     #close connection
     conn.close()
     editor.destroy()
-
 
 def edit():
     global editor
@@ -142,8 +217,7 @@ def edit():
     state_editor.grid(row=4, column=1)
     zipcode_editor = Entry(editor, width=30)
     zipcode_editor.grid(row=5, column=1)
-    # delete_box_editor = Entry(editor, width=30)
-    # delete_box_editor.grid(row=9, column=1, pady=5)
+    
     # create text box labels
     f_name_label_editor = Label(editor, text="First Name: ")
     f_name_label_editor.grid(row=0, column=0, pady=(10, 0))
@@ -157,8 +231,7 @@ def edit():
     state_label_editor.grid(row=4, column=0)
     zipcode_label_editor = Label(editor, text="Zipcode: ")
     zipcode_label_editor.grid(row=5, column=0)
-    # delete_box_label_editor = Label(editor, text="Select ID Number")
-    # delete_box_label_editor.grid(row=9, column=0, pady=5)
+   
     
     # loop through results
     for record in records:
@@ -232,17 +305,23 @@ query_btn = Button(root, text="Show Records", command= query)
 query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=134)
 
 # create a delete button
-delete_btn = Button(root, text="Delete Record", command=delete)
-delete_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=136)
+delete_btn = Button(root, text="Delete Record", command=delete, background="red", fg="#ffffff")
+delete_btn.grid(row=12, column=0, columnspan=2, pady=10, padx=10, ipadx=136)
 
 # create an update button
-update_btn = Button(root, text="Edit Record", command=edit)
-update_btn.grid(row=12, column=0, columnspan=2, pady=10, padx=10, ipadx=141)
+update_btn = Button(root, text="Edit Record", command=edit, background="yellow")
+update_btn.grid(row=11, column=0, columnspan=2, pady=10, padx=10, ipadx=141)
+
+# create a view button
+view_btn = Button(root, text="View Record", command=view, background="green", fg="#ffffff")
+view_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=140)
+
 
 #commit changes
 conn.commit()
+
 #close connection
 conn.close()
 
-# run the program
+
 root.mainloop()
